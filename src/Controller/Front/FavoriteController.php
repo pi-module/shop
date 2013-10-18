f487d9e2b859a6bb960d3103b971121809c184d3
voltan <djvoltan@gmail.com>
2013-10-18 16:14:40 +0330

    Add empty controllers

---
+++ src/Controller/Front/FavoriteController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt New BSD License
+ */
+
+/**
+ * @author Hossein Azizabadi <azizabadi@faragostaresh.com>
+ */
+
+namespace Module\Shop\Controller\Front;
+
+use Pi;
+use Pi\Mvc\Controller\ActionController;
+
+class FavoriteController extends ActionController
+{
+	public function indexAction()
+    {
+    	$this->view()->setTemplate('empty');
+    }
+}
