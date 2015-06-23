1c27f232be25f6fca7347d52673289cb7e1ae275
voltan <djvoltan@gmail.com>
2015-06-23 14:44:56 +0430

    Add lost file

---
+++ src/Controller/Admin/DiscountController.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt New BSD License
+ */
+
+/**
+ * @author Hossein Azizabadi <azizabadi@faragostaresh.com>
+ */
+namespace Module\Shop\Controller\Admin;
+
+use Pi;
+use Pi\Mvc\Controller\ActionController;
+use Module\Shop\Form\DiscountForm;
+use Module\Shop\Form\DiscountFilter;
+
+class DiscountController extends ActionController
+{
+    public function indexAction()
+    {
+        // Get info
+        $list = array();
+        $order = array('id DESC');
+        $select = $this->getModel('discount')->select()->order($order);
+        $rowset = $this->getModel('discount')->selectWith($select);
+        // Make list
+        foreach ($rowset as $row) {
+            $list[$row->id] = $row->toArray();
+        }
+        // Go to update page if empty
+        if (empty($list)) {
+            return $this->redirect()->toRoute('', array('action' => 'update'));
+        }
+        // Set view
+        $this->view()->setTemplate('discount-index');
+        $this->view()->assign('list', $list);
+    }
+
+    public function updateAction()
+    {
+        // Get id
+        $id = $this->params('id');
+        // Set form
+        $form = new DiscountForm('discount');
+        $form->setAttribute('enctype', 'multipart/form-data');
+        if ($this->request->isPost()) {
+            $data = $this->request->getPost();
+            $form->setInputFilter(new DiscountFilter);
+            $form->setData($data);
+            if ($form->isValid()) {
+                $values = $form->getData();
+                // Save values
+                if (!empty($values['id'])) {
+                    $row = $this->getModel('discount')->find($values['id']);
+                } else {
+                    $row = $this->getModel('discount')->createRow();
+                }
+                $row->assign($values);
+                $row->save();
+                // Add log
+                $operation = (empty($values['id'])) ? 'add' : 'edit';
+                Pi::api('log', 'shop')->addLog('discount', $row->id, $operation);
+                // Check it save or not
+                $message = __('Discount data saved successfully.');
+                $this->jump(array('action' => 'index'), $message);
+            }
+        } else {
+            if ($id) {
+                $discount = $this->getModel('discount')->find($id)->toArray();
+                $form->setData($discount);
+            }
+        }
+        // Set view
+        $this->view()->setTemplate('discount-update');
+        $this->view()->assign('form', $form);
+        $this->view()->assign('title', __('Add discount'));
+    }
+}
