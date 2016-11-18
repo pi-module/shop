67368fcd8bb378b1c375a1f41c7f19d3d0ab1960
voltan <djvoltan@gmail.com>
2016-11-18 09:40:28 +0330

    Work on search

--- src/Api/Search.php
+++ src/Api/Search.php
@@ -27,6 +27,7 @@ class Search extends AbstractSearch
      */
     protected $searchIn = array(
         'title',
+        'subtitle',
         'text_summary',
         'text_description',
     );
@@ -55,13 +56,25 @@ class Search extends AbstractSearch
     /**
      * {@inheritDoc}
      */
-    protected function buildUrl(array $item)
+    protected function buildUrl(array $item, $table)
     {
-        $link = Pi::url(Pi::service('url')->assemble('shop', array(
-            'module' => $this->getModule(),
-            'controller' => 'product',
-            'slug' => $item['slug'],
-        )));
+        switch ($table) {
+            case 'category':
+                $link = Pi::url(Pi::service('url')->assemble('shop', array(
+                    'module' => $this->getModule(),
+                    'controller' => 'category',
+                    'slug' => $item['slug'],
+                )));
+                break;
+
+            case 'product':
+                $link = Pi::url(Pi::service('url')->assemble('shop', array(
+                    'module' => $this->getModule(),
+                    'controller' => 'product',
+                    'slug' => $item['slug'],
+                )));
+                break;
+        }
 
         return $link;
     }
@@ -85,5 +98,71 @@ class Search extends AbstractSearch
         }
 
         return $image;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function query(
+        $terms,
+        $limit  = 0,
+        $offset = 0,
+        array $condition = array()
+    ) {
+        $terms = (array) $terms;
+        $dataAll = array();
+        $countAll = 0;
+        // model list
+        $tableList = array(
+            'category', 'product'
+        );
+        // Make query on all tables
+        foreach ($tableList as $table) {
+            $model = Pi::model($table, $this->module);
+            $where = $this->buildCondition($terms, $condition);
+            $count = $model->count($where);
+            if ($count) {
+                $data = $this->fetchResult($model, $where, $limit, $offset, $table);
+                $dataAll = array_merge($dataAll, $data);
+                $countAll = $countAll + $count;
+            }
+        }
+        $result = $this->buildResult($countAll, $dataAll);
+        return $result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function fetchResult(
+        $model,
+        $where,
+        $limit = 0,
+        $offset = 0,
+        $table
+    ) {
+        $data = array();
+        $select = $model->select();
+        $select->where($where);
+        $select->columns(array_keys($this->meta));
+        $select->limit($limit)->offset($offset);
+        if ($this->order) {
+            $select->order($this->order);
+        }
+        $rowset = $model->selectWith($select);
+        foreach ($rowset as $row) {
+            $item = array();
+            foreach ($this->meta as $column => $field) {
+                $item[$field] = $row[$column];
+                if ('content' == $field) {
+                    $item[$field] = $this->buildContent($item[$field]);
+                }
+            }
+            $item['url'] = $this->buildUrl($item, $table);
+            $item['image'] = $this->buildImage($item);
+            $data[] = $item;
+        }
+
+        return $data;
     }
 }
