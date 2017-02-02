f4696f0692ab7656ef3da69771b46cd4317f876e
voltan <djvoltan@gmail.com>
2017-02-02 15:13:22 +0330

    Update search system by new changes on pi

--- src/Api/Search.php
+++ src/Api/Search.php
@@ -20,7 +20,10 @@ class Search extends AbstractSearch
     /**
      * {@inheritDoc}
      */
-    protected $table = 'product';
+    protected $table = array(
+        'product',
+        'category',
+    );
 
     /**
      * {@inheritDoc}
@@ -35,13 +38,13 @@ class Search extends AbstractSearch
      * {@inheritDoc}
      */
     protected $meta = array(
-        'id' => 'id',
-        'title' => 'title',
-        'text_summary' => 'content',
-        'time_create' => 'time',
-        'slug' => 'slug',
-        'image' => 'image',
-        'path' => 'path',
+        'id'            => 'id',
+        'title'         => 'title',
+        'text_summary'  => 'content',
+        'time_create'   => 'time',
+        'slug'          => 'slug',
+        'image'         => 'image',
+        'path'          => 'path',
     );
 
     /**
@@ -96,71 +99,5 @@ class Search extends AbstractSearch
         }
 
         return $image;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function query(
-        $terms,
-        $limit  = 0,
-        $offset = 0,
-        array $condition = array()
-    ) {
-        $terms = (array) $terms;
-        $dataAll = array();
-        $countAll = 0;
-        // model list
-        $tableList = array(
-            'category', 'product'
-        );
-        // Make query on all tables
-        foreach ($tableList as $table) {
-            $model = Pi::model($table, $this->module);
-            $where = $this->buildCondition($terms, $condition);
-            $count = $model->count($where);
-            if ($count) {
-                $data = $this->fetchResult($model, $where, $limit, $offset, $table);
-                $dataAll = array_merge($dataAll, $data);
-                $countAll = $countAll + $count;
-            }
-        }
-        $result = $this->buildResult($countAll, $dataAll);
-        return $result;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    protected function fetchResult(
-        $model,
-        $where,
-        $limit = 0,
-        $offset = 0,
-        $table
-    ) {
-        $data = array();
-        $select = $model->select();
-        $select->where($where);
-        $select->columns(array_keys($this->meta));
-        $select->limit($limit)->offset($offset);
-        if ($this->order) {
-            $select->order($this->order);
-        }
-        $rowset = $model->selectWith($select);
-        foreach ($rowset as $row) {
-            $item = array();
-            foreach ($this->meta as $column => $field) {
-                $item[$field] = $row[$column];
-                if ('content' == $field) {
-                    $item[$field] = $this->buildContent($item[$field]);
-                }
-            }
-            $item['url'] = $this->buildUrl($item, $table);
-            $item['image'] = $this->buildImage($item);
-            $data[] = $item;
-        }
-
-        return $data;
     }
 }
