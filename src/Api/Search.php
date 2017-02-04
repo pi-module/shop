a566b478e9082b25047a628cd880e93b482cf948
voltan <djvoltan@gmail.com>
2017-02-04 10:21:22 +0330

    Update search order

--- src/Api/Search.php
+++ src/Api/Search.php
@@ -57,6 +57,16 @@ class Search extends AbstractSearch
     /**
      * {@inheritDoc}
      */
+    protected $order = array(
+        'recommended DESC',
+        'time_create DESC',
+        'id DESC'
+    );
+
+
+    /**
+     * {@inheritDoc}
+     */
     protected function buildUrl(array $item, $table = '')
     {
         switch ($table) {
